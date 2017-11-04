b89a9b7a07fe3c8405d93baddbeabcb7bbc89916
xilou <2399232918@qq.com>
2017-11-04 10:33:15 +0800

    异地测试

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php
+﻿<?php
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+define('APP_DEBUG',false);
 // 定义应用目录
 define('APP_PATH','./Application/');
 // 关闭目录安全文件的生成
